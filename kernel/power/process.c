fae460f8a13c057ff84ebbadfbfd182c63d1718a
Tejun Heo <tj@kernel.org>
2011-11-21 12:32:24 -0800

    freezer: use dedicated lock instead of task_lock() + memory barrier
    Freezer synchronization is needlessly complicated - it's by no means a
    hot path and the priority is staying unintrusive and safe.  This patch
    makes it simply use a dedicated lock instead of piggy-backing on
    task_lock() and playing with memory barriers.
    
    On the failure path of try_to_freeze_tasks(), locking is moved from it
    to cancel_freezing().  This makes the frozen() test racy but the race
    here is a non-issue as the warning is printed for tasks which failed
    to enter frozen for 20 seconds and race on PF_FROZEN at the last
    moment doesn't change anything.
    
    This simplifies freezer implementation and eases further changes
    including some race fixes.
    
    Change-Id: Id2d7980c5358aed2bbb97aa172acde4cd3c7a96b
    Signed-off-by: Tejun Heo <tj@kernel.org>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -132,12 +132,9 @@ static int try_to_freeze_tasks(bool sig_only)
 
 		read_lock(&tasklist_lock);
 		do_each_thread(g, p) {
-			task_lock(p);
-			if (freezing(p) && !freezer_should_skip(p) &&
-				elapsed_msecs > 1000)
+			if (!wakeup && freezing(p) && !freezer_should_skip(p))
 				sched_show_task(p);
 			cancel_freezing(p);
-			task_unlock(p);
 		} while_each_thread(g, p);
 		read_unlock(&tasklist_lock);
 	} else {
