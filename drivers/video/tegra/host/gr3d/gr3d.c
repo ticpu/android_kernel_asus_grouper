cca40661a8a05f83839e6931391c792d57355cc1
Terje Bergstrom <tbergstrom@nvidia.com>
2012-06-15 14:48:47 +0300

    video: tegra: host: Exclude suspend/resume if disabled
    Compile power management suspend and resume functions only when
    CONFIG_PM is enabled.
    
    Change-Id: If349984d62ed002594ba60ac25cd4dddd956aa6c
    Signed-off-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-on: http://git-master/r/109425
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Mayuresh Kulkarni <mkulkarni@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- drivers/video/tegra/host/gr3d/gr3d.c
+++ drivers/video/tegra/host/gr3d/gr3d.c
@@ -232,6 +232,7 @@ static int __exit gr3d_remove(struct nvhost_device *dev)
 	return 0;
 }
 
+#ifdef CONFIG_PM
 static int gr3d_suspend(struct nvhost_device *dev, pm_message_t state)
 {
 	return nvhost_client_device_suspend(dev);
@@ -242,6 +243,7 @@ static int gr3d_resume(struct nvhost_device *dev)
 	dev_info(&dev->dev, "resuming\n");
 	return 0;
 }
+#endif
 
 struct nvhost_device *gr3d_device;
 
